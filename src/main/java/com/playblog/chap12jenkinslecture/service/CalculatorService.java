46a95a7ee264d3d32a04293d00c9874bfb418646
heechae <chaehee15@naver.com>
2025-07-02 10:19:34 +0900

    fix: service 수정

--- src/main/java/com/playblog/chap12jenkinslecture/service/CalculatorService.java
+++ src/main/java/com/playblog/chap12jenkinslecture/service/CalculatorService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-
     public int plusTwoNumbers(CalculatorDto calculatorDto) {
         return calculatorDto.getNum1() + calculatorDto.getNum2();
     }
