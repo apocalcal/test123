1674cad8429e7fce81220190de35af8a5cf71a25
heechae <chaehee15@naver.com>
2025-07-04 10:30:35 +0900

    fix: controler ser3333e

--- src/main/java/com/playblog/chap12jenkinslecture/controller/CalculatorController.java
+++ src/main/java/com/playblog/chap12jenkinslecture/controller/CalculatorController.java
@@ -18,7 +18,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm still alive!😁🚣‍♂️";
+        return "I'm still alive!😁🐋🐋🚣‍♂️";
     }
 
     @GetMapping("/plus")
